1ae06aeb9d5a6cc0b7b914e9331bb19abbf380f1
majj <aidear@163.com>
2014-09-11 21:45:32 +0800

    schema

--- public/schema/testCtrl.js
+++ public/schema/testCtrl.js
@@ -1,17 +1,20 @@
 
-angular.module('test',['schemaForm','ui.ace']).controller('TestCtrl', function($scope,$http){
+angular.module('test',['schemaForm']).controller('TestCtrl', function($scope,$http){
 
-  $scope.tests = [
+  //$scope.tests = [
+    /*
     { name: "Simple", data: 'data/simple.json' },
     { name: "Complex Key Support", data: 'data/complex-keys.json' },
     { name: "Array", data: 'data/array.json' },
     { name: "Tab Array", data: 'data/tabarray.json' },
     { name: "TitleMap Examples", data: 'data/titlemaps.json' },
     { name: "Kitchen Sink", data: 'data/sink.json' }
-  ];
+    */
+ // ];
 
-  $scope.selectedTest = $scope.tests[0];
+ // $scope.selectedTest = $scope.tests[0];
 
+    /*
   $scope.$watch('selectedTest',function(val){
     if (val) {
       $http.get(val.data).then(function(res){
@@ -23,13 +26,22 @@ angular.module('test',['schemaForm','ui.ace']).controller('TestCtrl', function($
       });
     }
   });
-
+*/
+       $http.get('schema_form.json').then(function(res){
+        $scope.schema = res.data.schema;
+        $scope.form   = res.data.form;
+       // $scope.schemaJson = JSON.stringify($scope.schema,undefined,2);
+     //   $scope.formJson   = JSON.stringify($scope.form,undefined,2);
+        $scope.modelData = res.data.model || {};
+      });
+      
+      
   $scope.decorator = 'bootstrap-decorator';
 
   $scope.itParses     = true;
   $scope.itParsesForm = true;
 
-
+/*
   $scope.$watch('schemaJson',function(val,old){
     if (val && val !== old) {
       try {
@@ -40,7 +52,8 @@ angular.module('test',['schemaForm','ui.ace']).controller('TestCtrl', function($
       }
     }
   });
-
+  */
+/*
   $scope.$watch('formJson',function(val,old){
     if (val && val !== old) {
       try {
@@ -51,11 +64,11 @@ angular.module('test',['schemaForm','ui.ace']).controller('TestCtrl', function($
       }
     }
   });
-
+*/
   $scope.pretty = function(){
     return JSON.stringify($scope.modelData,undefined,2,2);
   };
-
+/*
   $scope.log = function(msg){
     console.log("Simon says",msg);
   };
@@ -67,6 +80,7 @@ angular.module('test',['schemaForm','ui.ace']).controller('TestCtrl', function($
   $scope.say = function(msg) {
     alert(msg);
   };
+  */
 
   $scope.submitForm = function(form, model) {
     // First we broadcast an event so all fields validate themselves
@@ -76,7 +90,7 @@ angular.module('test',['schemaForm','ui.ace']).controller('TestCtrl', function($
         console.log(JSON.stringify(form));
         var v = {"table":"ttt", cols:model};
         console.log(JSON.stringify(v));
-      alert('You did it!');
+     // alert('You did it!');
     }
   }
 
